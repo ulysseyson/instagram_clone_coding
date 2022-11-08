0013c55f07f8a12af5925890139ddcaf72e0aeb5
ulysseyson <ulyseyson57@gmail.com>
2022-11-08 22:23:08 +0900

    feat :  Scroll action

--- lib/home_layout.dart
+++ lib/home_layout.dart
@@ -1,34 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomeLayout extends StatelessWidget {
-  const HomeLayout({super.key});
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({super.key, this.data, this.scroll});
+  final data;
+  final scroll;
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (c, i) {
-        return Column(
-          children: [
-            Image.network("https://codingapple1.github.io/kona.jpg"),
-            Container(
-              constraints: BoxConstraints(maxWidth: 600),
-              padding: EdgeInsets.all(20),
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('좋아요 100'),
-                  Text('글쓴이'),
-                  Text('글내용'),
-                ],
-              ),
-            )
-          ],
-        );
-      },
-      itemCount: 3,
+    if (widget.data.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: widget.data.length,
+        controller: widget.scroll,
+        itemBuilder: (context, index) {
+          return Article(data: widget.data[index]);
+        },
+      );
+    }
+  }
+}
+
+class Article extends StatelessWidget {
+  const Article({
+    super.key,
+    required this.data,
+  });
+
+  final data;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(data['image'] as String),
+        Container(
+          constraints: BoxConstraints(maxWidth: 600),
+          padding: EdgeInsets.all(20),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("좋아요 ${data["likes"]}개"),
+              Text('글쓴이 ${data['user'] as String}'),
+              Text(data['content'] as String),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone_coding/home_layout.dart';
 import 'package:instagram_clone_coding/scaffold/appbar.dart';
 import 'package:instagram_clone_coding/style.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -23,6 +25,51 @@ class _MyAppState extends State<MyApp> {
   var _tab = 0;
   final body = [HomeLayout(), Text('샵페이지')];
   // This widget is the root of your application.
+
+  var data = [];
+
+  var scroll = ScrollController();
+
+  getData() async {
+    try {
+      var result = await http
+          .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+      print(data);
+      setState(() {
+        data = jsonDecode(result.body);
+      });
+      print(data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  getMore() async {
+    try {
+      var result = await http
+          .get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+      print(data);
+      setState(() {
+        data.add(jsonDecode(result.body));
+      });
+      print(data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+
+    scroll.addListener(() {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        getMore();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +97,7 @@ class _MyAppState extends State<MyApp> {
           ),
         ],
       ),
-      body: body[_tab],
+      body: HomeLayout(data: data, scroll: scroll),
     );
   }
 }
